ec689758c1de1608b32ac2d124fdfda1bba29ce1
Thakurabhay2406 <abhaysinghbasti1991@gmail.com>
2025-08-18 14:27:47 +0530

    Add files via upload

---
+++ prime number 1 to 100.py
@@ -0,0 +1,9 @@
+
+for num in range(2, 101):  # Start from 2 (smallest prime)
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
